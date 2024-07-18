e16539243b81c6a65d776c27962c89b85e87ac8c
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-07-18 20:36:41 +0200

    Deploying fastapi in vercel

--- api-server-python/main.py
+++ api-server-python/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -7,10 +6,9 @@ import joblib
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,12 +29,11 @@ class PredictionRequest(BaseModel):
 def predict_survival(data: PredictionRequest):
     df = pd.DataFrame([data.dict()])
     probability = model.predict_proba(df)[0][1]  
- 
     if data.Sex == 1 and data.Age <= 18 and data.Pclass == 1:
         probability += 0.1  
     elif data.Sex == 0 and data.Pclass == 3:
-        probability -= 0.1  
-    probability = min(max(probability, 0), 1)  #
+        probability -= 0.1 
+    probability = min(max(probability, 0), 1)  
     return {"survival_chance": f"{probability * 100:.2f}%"}
 
 if __name__ == "__main__":
