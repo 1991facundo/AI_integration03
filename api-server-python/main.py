ba852623e67c10348c60811cc01eacecba69d5b0
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-07-19 17:21:44 +0200

    trying with google drive

--- api-server-python/main.py
+++ api-server-python/main.py
@@ -3,20 +3,28 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+import gdown
+import os
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-model = joblib.load('model.joblib')
+url = 'https://drive.google.com/uc?id=1cpeDIT5m0PKEjSRMaprGYWoKCyaadbSK'
+output = 'model.joblib'
+
+# Descargar archivo si no existe
+if not os.path.exists(output):
+    gdown.download(url, output, quiet=False)
+
+model = joblib.load(output)
 
 class PredictionRequest(BaseModel):
     Pclass: int
@@ -30,7 +38,7 @@ class PredictionRequest(BaseModel):
 @app.post("/predict")
 def predict_survival(data: PredictionRequest):
     df = pd.DataFrame([data.dict()])
-    probability = model.predict_proba(df)[0][1]  
+    probability = model.predict_proba(df)[0][1] 
     
     if data.Sex == 1 and data.Age <= 18 and data.Pclass == 1:
         probability += 0.1  
